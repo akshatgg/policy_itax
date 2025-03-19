857794b85a7b6a840f4f5c7135a4de9bd2e9d24d
akshatgg <akshatg9636@gmail.com>
2025-03-19 10:17:43 +0000

    basic str

---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+// src/theme.js
+const theme = {
+    primary: "#0050A2", // PolicyBazaar Blue
+    secondary: "#002E5D", // Dark Blue
+    accent: "#F29F05", // Yellow-Orange (for highlights)
+    background: "#F8F9FA", // Light Gray
+    textPrimary: "#333333", // Dark Gray for text
+    textSecondary: "#666666", // Lighter Gray for secondary text
+    success: "#28a745", // Green for success messages
+    error: "#dc3545", // Red for error messages
+  };
+  
+  export default theme;
+  
