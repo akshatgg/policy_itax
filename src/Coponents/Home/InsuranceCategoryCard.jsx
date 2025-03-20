55f7fa14bd6663001872e496932fa0423dfb8a07
akshatgg <akshatg9636@gmail.com>
2025-03-20 21:37:10 +0000

    Refactor InsuranceCategoryCard layout for improved discount display and centering

--- src/Coponents/Home/InsuranceCategoryCard.jsx
+++ src/Coponents/Home/InsuranceCategoryCard.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const InsuranceCategoryCard = ({ discount, icon, title, className }) => {
     return (
-        <div className={`group relative flex flex-col items-center p-4 bg-gray-50 rounded-lg hover:shadow-md transition-shadow cursor-pointer ${className}`}>
+        <div className={`group relative flex flex-col items-center pb-2 bg-gray-50 rounded-lg hover:shadow-md transition-shadow cursor-pointer ${className}`}>
             {discount && (
-                <div className="absolute top-2 left-2 bg-green-200 text-green-800 text-xs px-2 py-1 rounded-full">
+                <div className="absolute bg-green-200 text-green-800 text-xs w-full">
+                    <p className='text-center'>
+
                     {discount}
+                    </p>
                 </div>
             )}
-            <div className="mb-4">
+            <div className="mb-4 text-center">
                 {icon}
             </div>
             <div className="text-center text-gray-800 font-medium">
